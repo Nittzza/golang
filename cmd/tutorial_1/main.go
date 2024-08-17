89d436e4bec65fc26ffbcc1a6d83052804c678f8
Nittzza <nittyaganesh@gmail.com>
2024-08-17 21:23:29 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,23 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	var printValue string = "Hello World"
 	printMe(printValue)
 
-	var numerator int = 11
-	var denominator int = 2
-	var result int = intDivision(numerator, denominator)
+	var numerator int = 10
+	var denominator int = 0
 
-	fmt.Println(result)
+	var result, remainder, err = intDivision(numerator, denominator)
+
+	switch {
+	case err != nil:
+		fmt.Printf(err.Error())
+	case remainder == 0:
+		fmt.Printf("The result of the integer division is %v", result)
+	default:
+		fmt.Printf("The result of the integer division is %v with remainder %v", result, remainder)
+	}
 }
 
 func printMe(printValue string) {
 	fmt.Println(printValue)
 }
 
-func intDivision(numerator int, denominator int) int {
+func intDivision(numerator int, denominator int) (int, int, error) {
+	if denominator == 0 {
+		return 0, 0, errors.New("division by zero is not allowed")
+	}
 	var result int = numerator / denominator
-	return result
+	var remainder int = numerator % denominator
+	return result, remainder, nil
 }
