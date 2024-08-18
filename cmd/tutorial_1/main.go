1f251722c617571e61e2debdbc10735bb8df3869
Nittzza <nittyaganesh@gmail.com>
2024-08-18 23:53:59 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,38 +1,15 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var printValue string = "Hello World"
-	printMe(printValue)
 
-	var numerator int = 10
-	var denominator int = 0
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println(intArr)
 
-	var result, remainder, err = intDivision(numerator, denominator)
+	intSlice := []int32{4, 5, 6}
+	fmt.Printf("The length is %v with capacity %v", len(intSlice), cap(intSlice))
 
-	switch {
-	case err != nil:
-		fmt.Printf(err.Error())
-	case remainder == 0:
-		fmt.Printf("The result of the integer division is %v", result)
-	default:
-		fmt.Printf("The result of the integer division is %v with remainder %v", result, remainder)
-	}
-}
-
-func printMe(printValue string) {
-	fmt.Println(printValue)
-}
-
-func intDivision(numerator int, denominator int) (int, int, error) {
-	if denominator == 0 {
-		return 0, 0, errors.New("division by zero is not allowed")
-	}
-	var result int = numerator / denominator
-	var remainder int = numerator % denominator
-	return result, remainder, nil
+	intSlice = append(intSlice, 7)
+	fmt.Printf("\nThe length is %v with capacity %v", len(intSlice), cap(intSlice))
 }
