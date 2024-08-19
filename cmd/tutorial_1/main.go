10dd54792ba8c77023dc690063b85c2cefe3acc9
Nittzza <nittyaganesh@gmail.com>
2024-08-19 23:49:13 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -5,19 +5,19 @@ import "fmt"
 func main() {
 
 	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println(myMap) // Prints the empty map
+	fmt.Println(myMap)
 
-	var myMap2 = map[string]uint8{"Adam": 23, "Sarah": 45}
-	fmt.Println(myMap2["Adam"])
+	var myMap2 = map[string]uint8{"Nittya": 23, "Aaditya": 45}
+	fmt.Println(myMap2["Nittya"])
 
 	var age, ok = myMap2["Jason"]
 	if ok {
 		fmt.Printf("The age is %v", age)
-	} else {
 		fmt.Println("Invalid Name")
-	}
 
-	for name := range myMap2 {
-		fmt.Printf("Name: %v\n", name)
+		for name, age := range myMap2 {
+			fmt.Printf("Name: %v, Age: %v\n", name, age)
+		}
+
 	}
 }
