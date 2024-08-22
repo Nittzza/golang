4e8245c7228621a27417f433515f0648bb781833
Nittzza <nittyaganesh@gmail.com>
2024-08-22 23:54:54 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	var myString = "résumé"
+	var myString = []rune("résumé")
 
-	var indexed = myString[0]
+	var indexed = myString[1]
 
 	fmt.Printf("%v, %T\n", indexed, indexed)
 
