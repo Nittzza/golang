80a1c8047e396245a8b986f360774d1d37c633bf
Anad667 <nittyaganesh@gmail.com>
2024-08-28 23:02:16 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var p *int32 = new(int32)
-	var i int32
+	var thing1 = [5]float64{1, 2, 3, 4, 5}
 
-	fmt.Printf("The value p points to is: %v", *p)
-	fmt.Printf("\nThe value of i is: %v", i)
+	fmt.Printf("\nThe memory location of the thing1 array is: %p", &thing1)
 
-	p = &i
-	*p = 1
+	var result [5]float64 = square(thing1)
 
-	fmt.Printf("\nThe value p points to is: %v", *p)
-	fmt.Printf("\nThe value of i is: %v", i)
+	fmt.Printf("\nThe result is: %v", result)
 
-	var k int32 = 2
-	i = k
+	fmt.Printf("\nThe value of thing1 is: %v", thing1)
+}
+
+func square(thing2 [5]float64) [5]float64 {
+	fmt.Printf("\nThe memory location of the thing2 array is: %p", &thing2)
+
+	for i := range thing2 {
+		thing2[i] = thing2[i] * thing2[i] // Changed from thing2[i] * thing2[1] to thing2[i] * thing2[i]
+	}
+
+	return thing2
 }
