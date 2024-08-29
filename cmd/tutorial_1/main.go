07a988083db92003792b334332ba35db2f6f1b14
Anad667 <nittyaganesh@gmail.com>
2024-08-29 23:51:58 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -12,15 +12,13 @@ func main() {
 	var result [5]float64 = square(thing1)
 
 	fmt.Printf("\nThe result is: %v", result)
-
-	fmt.Printf("\nThe value of thing1 is: %v", thing1)
 }
 
 func square(thing2 [5]float64) [5]float64 {
 	fmt.Printf("\nThe memory location of the thing2 array is: %p", &thing2)
 
 	for i := range thing2 {
-		thing2[i] = thing2[i] * thing2[i] // Changed from thing2[i] * thing2[1] to thing2[i] * thing2[i]
+		thing2[i] = thing2[i] * thing2[i]
 	}
 
 	return thing2
