98587492885e0131ce992c289871089233ec7b9d
Anad667 <nittyaganesh@gmail.com>
2024-08-31 23:31:24 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -5,21 +5,25 @@ import (
 )
 
 func main() {
-	var thing1 = [5]float64{10, 20, 30, 40, 50} // Changed array values
+	var thing1 = [7]float64{5, 10, 15, 20, 25, 30, 35} // Changed array size and values
 
 	fmt.Printf("\nThe memory location of the thing1 array is: %p", &thing1)
 
-	var result [5]float64 = square(thing1)
+	var result [7]float64 = processArray(thing1)
 
 	fmt.Printf("\nThe result is: %v", result)
 	fmt.Printf("\nThe value of thing1 is: %v", thing1)
 }
 
-func square(thing2 [5]float64) [5]float64 {
+func processArray(thing2 [7]float64) [7]float64 {
 	fmt.Printf("\nThe memory location of the thing2 array is: %p", &thing2)
 
 	for i := range thing2 {
-		thing2[i] = thing2[i] * 2 // Changed logic to multiply by 2 instead of squaring
+		if thing2[i] > 20 {
+			thing2[i] = thing2[i] + 10 // If the value is greater than 20, add 10
+		} else {
+			thing2[i] = thing2[i] - 5 // Otherwise, subtract 5
+		}
 	}
 
 	return thing2
