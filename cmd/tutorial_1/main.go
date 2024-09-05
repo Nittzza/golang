aef366da200b7d3e0e3bb6ce336287d81cf01c88
Anad667 <nittyaganesh@gmail.com>
2024-09-05 23:50:02 +0530

    your commit message

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -9,19 +9,20 @@ import (
 var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
 
 func main() {
-	t0 := time.Now() // Start time
+	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+	t0 := time.Now()                 // Start time
 
-	for i := 0; i < len(dbData); i++ {
-		dbCall(i)
+	for i, data := range dbData {
+		dbCall(i, data)
 	}
 
-	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
+	fmt.Printf("\nTotal execution time: %v\n", time.Since(t0))
 }
 
-func dbCall(i int) {
-	// Simulate DB call delay
-	var delay float32 = rand.Float32()*2000 + 1000 // Generate a random delay between 1000 and 3000 ms
+func dbCall(i int, data string) {
+	// Simulate DB call delay between 1000 and 3000 ms
+	delay := rand.Intn(2000) + 1000
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 
-	fmt.Println("The result from the database is:", dbData[i])
+	fmt.Printf("The result from the database (ID %d): %s\n", i+1, data)
 }
